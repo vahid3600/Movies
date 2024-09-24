5de671ef5c76c1c4fb9ad25472e5008a84fb5dc1
Vahid <vahidtalaie3600@gmail.com>
2024-09-25 01:36:31 +0330

    Add compose to project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,12 +45,17 @@ android {
 
 dependencies {
 
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.material3)
+
     implementation(libs.hilt.android)
     kapt(libs.hilt.compiler)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
